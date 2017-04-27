e45e90ee5ee04df9e2ed59ef21c615820097a223
cjredmond <connor.redmond@gmail.com>
2017-04-27 13:36:48 -0400

    initial

---
+++ db/migrate/20170427173500_create_teams.rb
@@ -0,0 +1,12 @@
+class CreateTeams < ActiveRecord::Migration[5.0]
+  def change
+    create_table :teams do |t|
+      t.integer, :win
+      t.integer, :loss
+      t.string, :div
+      t.string :conf
+
+      t.timestamps
+    end
+  end
+end
